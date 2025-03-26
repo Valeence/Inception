78b6575163a4b04350baf9a98f41774160107774
Valeence <124591400+Valeence@users.noreply.github.com>
2025-03-27 00:28:12 +0100

    Add files via upload

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar # gérer WordPress sans passer par l'interface graphique.
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --allow-root
-./wp-cli.phar core install --url=localhost --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_MAIL" --allow-root
+./wp-cli.phar core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_MAIL" --allow-root
 
+# Créer l'utilisateur admin s'il n'existe pas
 if ! ./wp-cli.phar user get "$WP_ADMIN_USER" --allow-root > /dev/null 2>&1; then
     echo "Création de l'utilisateur admin..."
     ./wp-cli.phar user create "$WP_ADMIN" "$WP_ADMIN_MAIL" --role=administrator --user_pass="$WP_ADMIN_PASSWORD" --allow-root
